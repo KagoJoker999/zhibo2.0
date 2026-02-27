a776c6cf252a153bfdafd3f2b86c10f3ebe7bf5a
Kago <majingkai@163.com>
2026-02-28 00:36:00 +0800

    更新排品计算说明文字

--- js/ranking.js
+++ js/ranking.js
@@ -769,7 +769,7 @@ function generateRankingPage() {
                 <h2>📋 排品计算</h2>
                 <p>
                     <span style="color: #888;">从评分和库存表汇总数据，按配置规则进行排品计算。</span>
-                    <span style="color: yellow;">计算后需保存，才能有效执行影刀【控库存】操作。</span>
+                    <span style="color: yellow;">计算后需保存，才可执行影刀【控库存】操作。</span>
                     <span style="color: red; font-weight: bold;">更新排品后，需要执行排品对照，才可生成对照表。</span>
                 </p>
             </div>
